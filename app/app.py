bf0cb6ba1f25dad777b120313f36fc4382f43592
IuiF <i.fukazawa.602@gmail.com>
2023-12-02 07:31:01 +0000

    Add upload route and update button link in upload.html

--- app/app.py
+++ app/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def upload():
+    return render_template("upload.html")
+
+
+@app.route("/index")
+def index():
     return render_template("index.html")
 
 
