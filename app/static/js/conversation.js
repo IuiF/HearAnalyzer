cbc56db7251dde49914909c4c0dadbdda4493350
IuiF <i.fukazawa.602@gmail.com>
2023-12-02 15:10:47 +0000

    Refactor code and update HTML layout

--- app/static/js/conversation.js
+++ app/static/js/conversation.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     fetch('/transcriptions')
         .then(response => response.json())
         .then(data => {
-            const container = document.getElementById('data-container');
+            const container = document.getElementById('conversation');
             data.forEach(item => {
                 const div = document.createElement('div');
                 div.textContent = `Speaker: ${item.speaker}, Text: ${item.text}`;
