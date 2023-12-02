e34d06768ee340a4c284c83530ba9c6b7edba8ed
yuh-dot <xue11pow0@gmail.com>
2023-12-02 22:17:09 +0900

    adapting input

--- app/app.py
+++ app/app.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from ..utils.VideoDownloader import VideoDownloader
+from ..utils.TextAnalyzer import TextAnalyzer
 
 app = Flask(__name__)
+
+def youtube_analize(url):
+    dl = VideoDownloader()
+    dl.download_video(video_link=url, output_path='tmp/')
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    input = 'world'
+    return render_template("upload.html", input = input)
 
+@app.route("/index", methods=['POST'])
+def index():
+    input2 = request.form['link1']
+    return render_template("index.html", input2 = input2)
+
+# @app.route("/tmpp", methods=['POST'])
+# def tmp():
+#     link1 = request.form['link1']
+#     return render_template("tmp.html", link1 = link1)
 
 if __name__ == "__main__":
     app.run(debug=True)
