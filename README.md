dcc1f5d4c9a95d248035753b07e948fbd0c2e7ec
IuiF <i.fukazawa.602@gmail.com>
2023-12-02 06:48:04 +0000

    Add .json and .ipynb files to .gitignore

--- README.md
+++ README.md
@@ -31,6 +31,14 @@ $ sudo apt-get install ffmpeg
 
 ### 以下の規約に同意した上で AccessToken を用意してください
 
+pyannote/speaker-diarization-3.1
+
 [pyannote/speaker-diarization](https://huggingface.co/pyannote/speaker-diarization)
 
 [pyannote/segmentation](https://huggingface.co/pyannote/segmentation)
+
+## GCP Translation API を使用しています
+### サービスアカウントキーをJSONファイルを任意のディレクトリに配置してから.envを書き換えてください
+
+[GCP Translation](https://cloud.google.com/translate/?hl=ja)
+
--- utils/AudioTranscriber.py
+++ utils/AudioTranscriber.py
@@ -1,18 +1,15 @@
 import os
 import tempfile
-import csv
 from pydub import AudioSegment
 import whisper
 import torch
 from pyannote.audio import Pipeline
 import sqlite3
+import six
+from google.cloud import translate_v2 as translate
 
 
 def create_database(db_path):
-    # 既に存在する場合は削除
-    if os.path.exists(db_path):
-        os.remove(db_path)
-
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
@@ -24,7 +21,8 @@ def create_database(db_path):
             start REAL,
             end REAL,
             speaker TEXT,
-            text TEXT
+            text TEXT,
+            translation TEXT
         )
     """
     )
@@ -50,17 +48,25 @@ class AudioTranscriber:
         db_path="tmp_transcription.db",
         model_name="base",
         token_key=None,
+        gcp_path=None,
     ):
         self.input_path = input_path
         self.db_path = db_path
         self.model_name = model_name
         self.token_key = token_key
+
+        # DBが既に存在する場合は削除
+        if os.path.exists(self.db_path):
+            os.remove(self.db_path)
         create_database(self.db_path)
+
+        self.gcp_path = gcp_path
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.gcp_path
 
     def process(self):
         # モデルのロード
         pipeline = Pipeline.from_pretrained(
-            "pyannote/speaker-diarization", use_auth_token=self.token_key
+            "pyannote/speaker-diarization-3.1", use_auth_token=self.token_key
         )
         pipeline = pipeline.to(torch.device("cuda"))
         model = whisper.load_model(self.model_name)
@@ -72,49 +78,52 @@ class AudioTranscriber:
         conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
 
-        # 結果をCSVファイルに書き込む準備
-        with open(self.output_path, mode="w", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Start", "End", "Speaker", "Text"])
+        # 各セグメントの処理
+        for segment, _, speaker in diarization.itertracks(yield_label=True):
+            start_ms = int(segment.start * 1000)
+            end_ms = int(segment.end * 1000)
+            audio = AudioSegment.from_file(self.input_path)[start_ms:end_ms]
 
-            # 各セグメントの処理
-            for segment, _, speaker in diarization.itertracks(yield_label=True):
-                start_ms = int(segment.start * 1000)
-                end_ms = int(segment.end * 1000)
-                audio = AudioSegment.from_file(self.input_path)[start_ms:end_ms]
+            # 一時ファイルにセグメントを保存
+            with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_file:
+                audio.export(tmp_file.name, format="wav")
+                tmp_file_path = tmp_file.name
 
-                # 一時ファイルにセグメントを保存
-                with tempfile.NamedTemporaryFile(
-                    suffix=".wav", delete=False
-                ) as tmp_file:
-                    audio.export(tmp_file.name, format="wav")
-                    tmp_file_path = tmp_file.name
+            # 一時ファイルから文字起こしを行う
+            text_en = model.transcribe(tmp_file_path)["text"]
+            if isinstance(text_en, six.binary_type):
+                text_en = text_en.decode("utf-8")
 
-                # 一時ファイルから文字起こしを行う
-                text = model.transcribe(tmp_file_path)["text"]
-                # print(
-                #     f'[{segment.start:03.1f}s - {segment.end:03.1f}s] {speaker}: {text}'
-                # )
+            # Translate Clientの生成
+            translate_client = translate.Client()
 
-                # トランスクリプションをデータベースに保存
+            # 英語テキストを日本語に翻訳
+            translation = translate_client.translate(text_en, target_language="ja")
+            translated_text = translation["translatedText"]
+
+            # トランスクリプションをデータベースに保存
+            cursor.execute(
+                """
+                INSERT INTO transcriptions (start, end, speaker, text, translation)
+                VALUES (?, ?, ?, ?, ?)
+            """,
+                (segment.start, segment.end, speaker, text_en, translated_text),
+            )
+
+            # 音声セグメントをデータベースに保存
+            with open(tmp_file_path, "rb") as audio_file:
+                audio_data = audio_file.read()
                 cursor.execute(
                     """
-                    INSERT INTO transcriptions (start, end, speaker, text)
-                    VALUES (?, ?, ?, ?)
+                    INSERT INTO audio_segments (audio)
+                    VALUES (?)
                 """,
-                    (segment.start, segment.end, speaker, text),
+                    (audio_data,),
                 )
 
-                # 音声セグメントをデータベースに保存
-                with open(tmp_file_path, "rb") as audio_file:
-                    audio_data = audio_file.read()
-                    cursor.execute(
-                        """
-                        INSERT INTO audio_segments (audio)
-                        VALUES (?)
-                    """,
-                        (audio_data,),
-                    )
+            # 一時ファイルを削除
+            os.remove(tmp_file_path)
 
-                # 一時ファイルを削除
-                os.remove(tmp_file_path)
+        # 変更をコミットし、接続を閉じる
+        conn.commit()
+        conn.close()
